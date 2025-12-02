b39972aeea2b3f9fccbdaeb36ab50ef6faab0c41
DokiDokiDesu <demre421423@hotmail.com>
2025-12-02 22:44:04 +0300

    project created. tailwind config added.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Homepage } from "./pages/homepage";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
